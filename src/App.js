96384610268bd4920a584c9bc16d2c6552fde95f
blehthebob <ruiqi.zhang@gmail.com>
2024-02-03 23:04:38 +0000

    losing my mind

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const [msg, setMsg] = useState([]);
+    useEffect(() => {
+        msgs();
+    }, []);
+    const msgs = async () => {
+        const response = await fetch('/api/auth/login');
+        const data = await response.json();
+        setMsg(data);
+    }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +20,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {msg.map((m, i) => (
+            <p key={i}>{m}</p>
+        ))}
         <a
           className="App-link"
           href="https://reactjs.org"
